d8ea972080e75b060aed6071c03fc4ffb8fa8324
Nghia Nguyen <huunghia160799@gmail.com>
2020-04-24 10:12:01 +0700

    Update error handling in models

--- GreedyInfoMax/utils/model_utils.py
+++ GreedyInfoMax/utils/model_utils.py
@@ -147,15 +147,18 @@ def reload_weights(opt, model, optimizer, reload_model, patch_idx=None):
                     )
                 )
             else:
-                optim.load_state_dict(
-                    torch.load(
-                        os.path.join(
-                            opt.model_path,
-                            "optim_{}_{}_{}.ckpt".format(patch_idx, str(i), opt.start_epoch),
-                        ),
-                        map_location=opt.device.type,
+                try:
+                    optim.load_state_dict(
+                        torch.load(
+                            os.path.join(
+                                opt.model_path,
+                                "optim_{}_{}_{}.ckpt".format(patch_idx, str(i), opt.start_epoch),
+                            ),
+                            map_location=opt.device.type,
+                        )
                     )
-                )
+                except RuntimeError as e:
+                    print(e)
     else:
         print("Randomly initialized model")
 
